3ff14346c3e5d03e73a2d6691934e1c7f246e9f4
Ritesh <ritesh805045@gmail.com>
2025-02-22 22:06:47 +0530

    adding logout buttom

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-activeRoute: any;
-menu() {
-throw new Error('Method not implemented.');
-}
+export class HeaderComponent implements OnInit{
 
+  showLogout: boolean = false;
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Define the routes where the logout button should be visible
+        const logoutRoutes = ['/branch/available', '/master/product/addProduct', '/branch/booked'];
+        this.showLogout = logoutRoutes.includes(event.url);
+      }
+    });
+  }
 
-  logout(){
-
+  logout() {
+    this.router.navigate(['/login']);
   }
 }
